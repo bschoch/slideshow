1ac0ff169e96d1ddb99b62c9dc34086fbd255e84
ryanirilli <ryanirilli@gmail.com>
2016-03-28 16:00:26 -0700

    clean up api

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 var express = require('express');
+var serverSettings = {
+  port: process.env.PORT || 3000
+};
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (serverSettings.port));
 
-app.get('/', function(request, response) {
-  response.json({ message: 'Visaudio API' });
-});
-app.post('/lives', function(request, response) {
-      var body = '';
-      request.on('data', function (data) {
-          body += data;
-
-          // Too much POST data, kill the connection!
-          if (body.length > 1e6)
-              request.connection.destroy();
-      });
-      request.on('end', function () {
-          var post = qs.parse(body);
-      });
+app.post('/generate-slideshow', (request, response) => {
+  //do slideshow magic
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(serverSettings.port, () => {
+  console.log(`Server listening on port ${serverSettings.port}`);
 });
+
 
 
