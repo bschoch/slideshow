d471b428ca55389c1d916c441983e474d32072e1
Bradley Schoch <bschoch@gmail.com>
2016-04-05 18:35:25 -0700

    updating hash

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var slideshow = require('./slideshow.js')
 var facebook = require('./facebook.js')
 
-facebook.getPhotos({"token": "CAAXQlOvZBFXcBADZBnWUru0hutjcb9IMGCoyKitEMbvuJU9MBnmXHVKjggjeIz7rOV2UAbq0nnscbFJhELdj8CgZAJvH1rptPQv8XrSTpbDZArPIHuLyWzkNpfG2svDigpZAZAthNC4qEbKZCXDZC62E8J4niiFJszTsLDARZBKuQLCK0p3iZCm5osDYVYkXBzZA6IbOZBUGYRPRQQZDZD"}).then(function () {
+//facebook.getPhotos({"token": "CAAXQlOvZBFXcBADZBnWUru0hutjcb9IMGCoyKitEMbvuJU9MBnmXHVKjggjeIz7rOV2UAbq0nnscbFJhELdj8CgZAJvH1rptPQv8XrSTpbDZArPIHuLyWzkNpfG2svDigpZAZAthNC4qEbKZCXDZC62E8J4niiFJszTsLDARZBKuQLCK0p3iZCm5osDYVYkXBzZA6IbOZBUGYRPRQQZDZD"}).then(function () {
     slideshow.create({}).then(function () {
         facebook.uploadVideo({})
     })
-})
+//})
