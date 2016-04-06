af4d58660a4b9c4a303b8d157b0a3b9b21c61531
Bradley Schoch <bschoch@gmail.com>
2016-04-05 18:34:32 -0700

    making ffmpeg synchronous

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,29 +1,15 @@
 var spawn = require('child_process').spawn
+var spawnSync = require('child_process').spawnSync
 var q = require('q')
+var sync = true
 
 exports.imageToVideo = imageToVideo
 
 function imageToVideo(imagePath, duration, destination, width, height) {
   var deferred = q.defer()
-  var command = spawn('ffmpeg', ['-loop', '1', '-i', imagePath, '-y', '-an', '-r', '25', '-t', duration, '-vf', 'scale=' + width + 'x' + height + ',setsar=1:1', destination])
-  //var stderr
-  //command.stderr.on('data', function (chunk) {
-  //  if (chunk) {
-  //    stderr += chunk.toString()
-  //  }
-  //})
-  //command.stderr.on('end', function () {
-  //  console.log(stderr)
-  //})
-  command.on('exit', function (code, signal) {
-    if (signal) {
-      return deferred.reject(signal)
-    } else if (code) {
-      return deferred.reject(code)
-    } else {
-      return deferred.resolve('converted ' + imagePath + ' to ' + destination + ' for duration ' + duration)
-    }
-  })
+  var args = ['-loop', '1', '-i', imagePath, '-y', '-an', '-r', '25', '-t', duration, '-vf', 'scale=' + width + 'x' + height + ',setsar=1:1', destination]
+  var success = 'converted ' + imagePath + ' to ' + destination + ' for duration ' + duration
+  call(deferred, args, success)
   return deferred.promise
 }
 
@@ -36,25 +22,9 @@ function concatenateVideos(videoPaths, destination) {
     args.push('-i')
     args.push(videoPath)
   })
-  var command = spawn('ffmpeg', args.concat(['-y', '-filter_complex', 'concat=n=' + videoPaths.length + ':v=1:a=0', destination]))
-  //var stderr
-  //command.stderr.on('data', function (chunk) {
-  //  if (chunk) {
-  //    stderr += chunk.toString()
-  //  }
-  //})
-  //command.stderr.on('end', function () {
-  //  console.log(stderr)
-  //})
-  command.on('exit', function (code, signal) {
-    if (signal) {
-      return deferred.reject(signal)
-    } else if (code) {
-      return deferred.reject(code)
-    } else {
-      return deferred.resolve('concatenated ' + videoPaths.length + ' video(s) to ' + destination)
-    }
-  })
+  args = args.concat(['-y', '-filter_complex', 'concat=n=' + videoPaths.length + ':v=1:a=0', destination])
+  var success = 'concatenated ' + videoPaths.length + ' video(s) to ' + destination
+  call(deferred, args, success)
   return deferred.promise
 }
 
@@ -62,24 +32,30 @@ exports.addAudioToVideo = addAudioToVideo
 
 function addAudioToVideo(videoPath, audioPath, destination) {
   var deferred = q.defer()
-  var command = spawn('ffmpeg', ['-i', videoPath, '-i', audioPath, '-y', '-codec', 'copy', '-shortest', '-strict', '-2', destination])
-  //var stderr
-  //command.stderr.on('data', function (chunk) {
-  //  if (chunk) {
-  //    stderr += chunk.toString()
-  //  }
-  //})
-  //command.stderr.on('end', function () {
-  //  console.log(stderr)
-  //})
-  command.on('exit', function (code, signal) {
-    if (signal) {
-      return deferred.reject(signal)
-    } else if (code) {
-      return deferred.reject(code)
-    } else {
-      return deferred.resolve('combined audio ' + audioPath + ' and video ' + videoPath + ' to ' + destination)
-    }
-  })
+  var args = ['-i', videoPath, '-i', audioPath, '-y', '-codec', 'copy', '-shortest', '-strict', '-2', destination]
+  var success = 'combined audio ' + audioPath + ' and video ' + videoPath + ' to ' + destination
+  call(deferred, args, success)
   return deferred.promise
+}
+
+function call(deferred, args, success) {
+  if (sync) {
+    var result = spawnSync('ffmpeg', args)
+    if (result.status === 0) {
+      deferred.resolve(success)
+    } else {
+      deferred.reject([result.stdout ? result.stdout.toString() : '', result.stderr ? result.stderr.toString() : ''])
+    }
+  } else {
+    var command = spawn('ffmpeg', args)
+    command.on('exit', function (code, signal) {
+      if (signal) {
+        return deferred.reject(signal)
+      } else if (code) {
+        return deferred.reject(code)
+      } else {
+        return deferred.resolve(success)
+      }
+    })
+  }
 }
